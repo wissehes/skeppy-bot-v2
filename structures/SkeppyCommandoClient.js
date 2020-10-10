1d30f61d9a3e12686a712782a7ccbf16ccfda57f
Wisse <wissehes@gmail.com>
2020-10-10 13:22:34 +0200

    Use the keyv as settings provider

--- structures/SkeppyCommandoClient.js
+++ structures/SkeppyCommandoClient.js
@@ -1,12 +1,18 @@
 const { KSoftClient } = require("@ksoft/api");
 const { CommandoClient } = require("discord.js-commando");
 
+const Keyv = require("keyv");
+const KeyvProvider = require("commando-provider-keyv");
 class SkeppyCommandoClient extends CommandoClient {
   constructor(options) {
     super(options);
 
     this.config = options.config;
     this.ksoft = new KSoftClient(this.config.api.ksoft);
+
+    this.settingsKeyv = new Keyv("sqlite://settings.sqlite");
+
+    this.setProvider(new KeyvProvider(this.settingsKeyv));
   }
 }
 
