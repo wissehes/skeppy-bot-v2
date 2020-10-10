783b7437eadcb9a4f0e238a67e808466a55121a1
Wisse <wissehes@gmail.com>
2020-10-10 22:01:26 +0200

    Create script to copy old scores database to new database

---
+++ db/scripts/CopyScoresFromOldDB.js
@@ -0,0 +1,33 @@
+/**
+ * This script copies all of the old scores to the new database
+ */
+
+const SQLite = require("better-sqlite3");
+
+const sql = new SQLite(process.argv[2]);
+
+let all;
+
+try {
+  all = sql.prepare("SELECT * FROM scores").all();
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+}
+
+const connect = require("../connect");
+
+connect(require("../../config").mongoURI);
+
+const Point = require("../models/Point");
+
+const mapped = all.map((s) => ({
+  guildID: s.guild,
+  userID: s.user,
+  points: s.points,
+  level: s.level,
+}));
+
+Point.insertMany(mapped);
+
+console.log(mapped);
