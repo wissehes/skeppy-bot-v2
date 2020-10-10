e36a9c88df608c4754f1dbdfed58b8bf316437f4
Wisse <wissehes@gmail.com>
2020-10-10 14:28:48 +0200

    Connect to MongoDB

--- structures/SkeppyCommandoClient.js
+++ structures/SkeppyCommandoClient.js
@@ -3,6 +3,9 @@ const { CommandoClient } = require("discord.js-commando");
 
 const Keyv = require("keyv");
 const KeyvProvider = require("commando-provider-keyv");
+
+const connectToMongoDB = require("../db/connect");
+
 class SkeppyCommandoClient extends CommandoClient {
   constructor(options) {
     super(options);
@@ -13,6 +16,8 @@ class SkeppyCommandoClient extends CommandoClient {
     this.settingsKeyv = new Keyv("sqlite://db/settings.sqlite");
 
     this.setProvider(new KeyvProvider(this.settingsKeyv));
+
+    connectToMongoDB(this.config.mongoURI);
   }
 }
 
