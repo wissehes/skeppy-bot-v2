722bb204e7418248cdd21ab3f1eda0a96eff79e7
Wisse <wissehes@gmail.com>
2020-10-10 14:43:48 +0200

    Add SkeppyBraincells to client

--- structures/SkeppyCommandoClient.js
+++ structures/SkeppyCommandoClient.js
@@ -6,18 +6,23 @@ const KeyvProvider = require("commando-provider-keyv");
 
 const connectToMongoDB = require("../db/connect");
 
+const SkeppyBraincells = require("./SkeppyBraincells");
+
 class SkeppyCommandoClient extends CommandoClient {
   constructor(options) {
     super(options);
 
+    // Bind config to this
     this.config = options.config;
+    // Bind ksoft to this
     this.ksoft = new KSoftClient(this.config.api.ksoft);
+    // Bind SkeppyBraincells to this
+    this.braincells = new SkeppyBraincells(this);
 
-    this.settingsKeyv = new Keyv("sqlite://db/settings.sqlite");
-
-    this.setProvider(new KeyvProvider(this.settingsKeyv));
-
+    // DB stuff
     connectToMongoDB(this.config.mongoURI);
+    this.settingsKeyv = new Keyv("sqlite://db/settings.sqlite");
+    this.setProvider(new KeyvProvider(this.settingsKeyv));
   }
 }
 
