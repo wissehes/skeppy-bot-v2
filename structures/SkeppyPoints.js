a96d46d827f8ce2bdfe807c538840d4efbdbbd8f
Wisse <wissehes@gmail.com>
2020-10-11 13:39:04 +0200

    Create method to generate the rank card with Canvacord

--- structures/SkeppyPoints.js
+++ structures/SkeppyPoints.js
@@ -2,6 +2,8 @@ const { Guild, GuildMember, MessageAttachment } = require("discord.js");
 const Point = require("../db/models/Point");
 const PointsUtils = require("../utils/PointsUtils");
 
+const { Rank } = require("canvacord");
+
 class SkeppyPoints {
   constructor(client) {
     this.client = client;
@@ -41,11 +43,69 @@ class SkeppyPoints {
     const points = await this.get(guild, member);
 
     points.points += amount;
-    points.level = PointsUtils.generateLevel(points.points);
+    points.level = PointsUtils.calculateLevel(points.points);
 
     await points.save();
-
     return points;
+  }
+
+  /**
+   * Generate a member's xp card
+   * @param {Guild} guild The guild the member is in
+   * @param {GuildMember} member The member
+   */
+  async generateCard(guild, member) {
+    const userPoints = await this.get(guild, member);
+    const cardData = await this.generateCardData(userPoints);
+
+    const card = new Rank()
+      .setAvatar(member.user.displayAvatarURL({ format: "png" }))
+      .setCurrentXP(cardData.currentXP)
+      .setRequiredXP(cardData.neededXP)
+      .setLevel(userPoints.level)
+      .setRank(cardData.rank)
+      .setStatus(member.presence.status)
+      .setProgressBar(["#FF0000", "#0000FF"], "GRADIENT")
+      .setUsername(member.user.username)
+      .setDiscriminator(member.user.discriminator);
+
+    const built = await card.build();
+
+    const attachment = new MessageAttachment(built, "card.png");
+    return attachment;
+  }
+
+  /**
+   * Generate the card data
+   * @param {object} userPoints The points received from MongoDB
+   */
+  async generateCardData(userPoints) {
+    // Get the current rank of the user
+    const rank = await PointsUtils.calculateRank(userPoints);
+
+    // Get the points that are needed for the current level
+    const neededPointsForLevel = PointsUtils.calculatePointsForLevel(
+      userPoints.level
+    );
+
+    // Get the points that are needed to level up
+    const neededPointsForLevelUp = PointsUtils.calculateNeededPoints(
+      userPoints.points
+    );
+
+    // Subtract the points that are needed from
+    // the current level from the user's points
+    const currentPoints = userPoints.points - neededPointsForLevel;
+
+    // Add the needed points to level up to the currentpoints
+    const neededPoints = neededPointsForLevelUp + currentPoints;
+
+    return {
+      rank,
+      level: userPoints.level,
+      neededXP: neededPoints,
+      currentXP: currentPoints,
+    };
   }
 }
 
