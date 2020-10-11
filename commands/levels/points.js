cb6c2a34bb24805e9e6c6f850883e4b8fcba7151
Wisse <wissehes@gmail.com>
2020-10-11 13:39:10 +0200

    Create points command

---
+++ commands/levels/points.js
@@ -0,0 +1,35 @@
+const SkeppyCommand = require("../../structures/SkeppyCommand");
+
+module.exports = class PointsCommand extends SkeppyCommand {
+  constructor(client) {
+    super(client, {
+      name: "points",
+      memberName: "points",
+      aliases: ["xp", "rank", "level"],
+      group: "levels",
+      description: "Shows your points!",
+      args: [
+        {
+          key: "member",
+          prompt: "Who's points do you want to see?",
+          type: "member",
+          default: (m) => m.member,
+        },
+      ],
+    });
+  }
+
+  async run(message, { member }) {
+    message.channel.startTyping();
+
+    try {
+      const card = await this.client.points.generateCard(message.guild, member);
+
+      message.channel.send(card);
+      message.channel.stopTyping();
+    } catch (e) {
+      message.channel.stopTyping();
+      throw e;
+    }
+  }
+};
