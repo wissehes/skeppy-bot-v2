ee6b5d8e16cf329144ba622e03d5a2e49ebb57b8
Wisse <wissehes@gmail.com>
2020-10-11 13:38:42 +0200

    Add calculation methods to the points utils

--- utils/PointsUtils.js
+++ utils/PointsUtils.js
@@ -1,6 +1,66 @@
+const Point = require("../db/models/Point");
+
 class PointsUtils {
-  static generateLevel(points) {
+  /**
+   * Generate the level
+   * @param {Number} points the amount of points
+   */
+  static calculateLevel(points) {
     return Math.floor(0.1 * Math.sqrt(points));
+  }
+
+  /**
+   * Calculate the amount of points a level requires
+   * @param {number} level the level
+   */
+  static calculatePointsForLevel(level) {
+    let points = 0;
+    let currentLevel = 0;
+
+    while (currentLevel != level) {
+      points++;
+      currentLevel = this.calculateLevel(points);
+    }
+    return points;
+  }
+
+  /**
+   * Calculate the amount of points that are needed to level up
+   * @param {number} points Amount of points
+   */
+  static calculateNeededPoints(points) {
+    let currentLevel = this.calculateLevel(points);
+
+    const nextLevel = this.calculateLevel(points) + 1;
+
+    let neededPoints = 0;
+    while (currentLevel < nextLevel) {
+      neededPoints++;
+      currentLevel = this.calculateLevel(points + neededPoints);
+    }
+    return neededPoints;
+  }
+
+  /**
+   * Calculate the rank the user has
+   * @param {object} userPoints The points received from MongoDB
+   */
+  static async calculateRank(userPoints) {
+    const allUsers = await Point.find({
+      guildID: userPoints.guildID,
+    });
+
+    const sorted = allUsers.sort((a, b) => b.points - a.points);
+
+    const mappedWithRankNumber = sorted.map((a, i) => ({
+      userID: a.userID,
+      rank: i + 1,
+    }));
+
+    const { rank } = mappedWithRankNumber.find(
+      (a) => a.userID == userPoints.userID
+    );
+    return rank;
   }
 }
 
