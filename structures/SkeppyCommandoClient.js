367e143a2be6d8439c0957b6c851c41c9c00b69b
Wisse <wissehes@gmail.com>
2020-10-12 15:01:56 +0200

    Create presence functions

--- structures/SkeppyCommandoClient.js
+++ structures/SkeppyCommandoClient.js
@@ -9,6 +9,7 @@ const connectToMongoDB = require("../db/connect");
 const SkeppyBraincells = require("./SkeppyBraincells");
 const SkeppyPoints = require("./SkeppyPoints");
 
+const fs = require("fs");
 class SkeppyCommandoClient extends CommandoClient {
   constructor(options) {
     super(options);
@@ -26,6 +27,48 @@ class SkeppyCommandoClient extends CommandoClient {
     connectToMongoDB(this.config.mongoURI);
     this.settingsKeyv = new Keyv("sqlite://db/settings.sqlite");
     this.setProvider(new KeyvProvider(this.settingsKeyv));
+
+    // Presence stuff
+    this.defaultPresence = `with {guilds} servers | {prefix} help`;
+    this.presenceText = "";
+  }
+
+  /**
+   * Update the presence data
+   */
+  async updatePresence() {
+    this.loadPresence();
+
+    this.user.setActivity(this.formatPresence(this.presenceText));
+  }
+
+  /**
+   * Set a custom presence
+   * @param {string} text the presence to set
+   */
+  setPresence(text) {
+    this.presenceText = text;
+    this.user.setActivity(this.formatPresence(text));
+    fs.writeFileSync("./db/presence.txt", text);
+  }
+
+  /**
+   * Loads the presence
+   */
+  loadPresence() {
+    if (fs.existsSync("./db/presence.txt")) {
+      this.presenceText = fs.readFileSync("./db/presence.txt", "utf-8");
+    } else {
+      this.presenceText = this.defaultPresence;
+    }
+    return this.presenceText;
+  }
+
+  formatPresence(text) {
+    const guilds = this.guilds.cache.size;
+    return text
+      .replace("{guilds}", guilds)
+      .replace("{prefix}", this.commandPrefix);
   }
 }
 
