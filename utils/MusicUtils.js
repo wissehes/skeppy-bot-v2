a5b45ee6442e3941617e1443f637f75aa2a7e611
Wisse <wissehes@gmail.com>
2020-10-17 01:58:34 +0200

    Create better now playing embed

--- utils/MusicUtils.js
+++ utils/MusicUtils.js
@@ -24,19 +24,43 @@ class MusicUtils {
         currentTrack.requestedBy.avatarURL
       )
       .setTitle(currentTrack.info.title || "Unknown title")
-      .setColor("RANDOM").setDescription(`
-Length: **${currentTrack.info.length}**
-Requested by: **${requestedBy.username}**\`#${requestedBy.discriminator}\`
-`);
+      .setURL(currentTrack.info.uri)
+      .setColor("RANDOM");
+
+    embed.setFooter(
+      `Requested by ${requestedBy.tag} | ${queue.length} ${
+        queue.length > 1 ? "tracks" : "track"
+      } remaining in the queue...`
+    );
 
     return embed;
   }
 
   /**
-   * Format seconds to hh:mm:ss style
-   * @param {number} seconds The number of seconds
+   * Format milliseconds to hh:mm:ss style
+   * @param {number} millisec The number of milliseconds
    */
-  formatSeconds(seconds) {}
+  formatMS(millisec) {
+    // Credit: https://stackoverflow.com/questions/19700283/how-to-convert-time-milliseconds-to-hours-min-sec-format-in-javascript
+
+    var seconds = (millisec / 1000).toFixed(0);
+    var minutes = Math.floor(seconds / 60);
+    var hours = "";
+    if (minutes > 59) {
+      hours = Math.floor(minutes / 60);
+      hours = hours >= 10 ? hours : "0" + hours;
+      minutes = minutes - hours * 60;
+      minutes = minutes >= 10 ? minutes : "0" + minutes;
+    }
+
+    seconds = Math.floor(seconds % 60);
+    seconds = seconds >= 10 ? seconds : "0" + seconds;
+    if (hours != "") {
+      return hours + ":" + minutes + ":" + seconds;
+    }
+
+    return minutes + ":" + seconds;
+  }
 }
 
 module.exports = MusicUtils;
