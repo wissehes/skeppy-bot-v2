d265f6fa60b370167bd6eb296476f937df47ff93
Wisse <wissehes@gmail.com>
2020-10-17 02:06:29 +0200

    Pass the skeppytrack to the queue handler

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,4 +1,5 @@
 const SkeppyCommand = require("../../structures/SkeppyCommand");
+const SkeppyTrack = require("../../structures/SkeppyTrack");
 
 module.exports = class PlayCommand extends SkeppyCommand {
   constructor(client) {
@@ -27,12 +28,17 @@ module.exports = class PlayCommand extends SkeppyCommand {
     const node = this.client.player.getNode();
 
     const results = await node.rest.resolve(song, "youtube");
-    if (!results) return;
+
+    if (!results) {
+      return message.reply("I couldn't find anything...");
+    }
+
+    const track = new SkeppyTrack(results.tracks[0], message.author);
 
     const dispatcher = await this.client.queue.handle({
       message,
       node,
-      track: results.tracks[0],
+      track,
     });
 
     if (dispatcher) {
