da9b22fc220a09c16c88eccf405f4cb73f723cb7
Wisse <wissehes@gmail.com>
2020-10-19 14:07:20 +0200

    Create new music perm utils since the pervious were ✨confusing✨

--- utils/MusicPermUtils.js
+++ utils/MusicPermUtils.js
@@ -3,124 +3,84 @@
 
 const { MessageEmbed } = require("discord.js");
 const { CommandoMessage } = require("discord.js-commando");
+const replies = require("../assets/replies.json");
 
 class MusicPermUtils {
   constructor() {
-    this.requirements = {
-      MEMBER_VC: {
-        description: "Requires a user to be in a voice channel",
-        test: (message) => {
-          return !!message.member.voice.id;
-        },
-      },
-      PLAYING: {
-        description: "Requires there to be a player in the guild",
-        test: (message) => {
-          return message.client.queue.has(message.guild.id);
-        },
-      },
-      SAME_VC: {
-        description:
-          "Requires the user to be in the same voice channel as the bot",
-        test: (message) => {
-          if (!message.member.voice.channel.id) return false;
+    this.tests = {
+      MEMBER_VC: (message) => !!message.member.voice.channel,
+      PLAYING: (message) => message.client.queue.has(message.guild.id),
+      SAME_VC: (message) => {
+        const dispatcher = message.client.queue.get(message.guild.id);
 
-          const dispatcher = message.client.queue.get(message.guild.id);
-          if (dispatcher) {
-            if (
-              dispatcher.player.voiceConnection.voiceChannelID ==
-              message.member.voice.channel.id
-            ) {
-              return true;
-            }
-          } else return false;
-        },
-      },
-    };
+        if (dispatcher) {
+          const clientVC = dispatcher.player.voiceConnection.voiceChannelID;
+          const memberVC = message.member.voice.channel.id;
 
-    this.permissions = {
-      ADD_SONGS: {
-        description: "Add songs to the queue and start the queue",
-        requires: ["MEMBER_VC", "SAME_VC"],
-      },
-      STOP_QUEUE: {
-        description: "Stop the queue from playing",
-        requires: ["MEMBER_VC", "PLAYING", "SAME_VC"],
+          return memberVC == clientVC;
+        } else return false;
       },
     };
   }
 
   /**
-   * Test for requirements
+   * Check if user can start or add songs to the queue
    * @param {CommandoMessage} message
-   * @param {String} permission
    */
-  test(message, permission) {
-    const perms = this.permissions[permission].requires;
-    const permsToCheck = perms.map((a) => this.requirements[a]);
-
-    return !permsToCheck.some((a) => !a.test(message));
-  }
-
-  /**
-   * Check if a user is allowed to add songs to the queue
-   * @param {CommandoMessage} message The message object
-   */
-  addSongs(message) {
-    const client = message.client;
-
-    if (!message.member.voice.channel) {
-      message.embed(this.errorEmbed("no_member_vc"));
+  canStartOrPlay(message) {
+    if (!this.tests.MEMBER_VC(message)) {
+      this.errorEmbed(message, "no_member_vc");
+      return false;
     }
 
-    const isPlaying = client.queue.get(message.guild.id);
-
-    if (isPlaying) {
-      if (
-        message.member.voice.channel.id ==
-        isPlaying.player.voiceConnection.voiceChannelID
-      ) {
-        return true;
-      } else {
-        message.embed(this.errorEmbed("different_vc"));
-      }
-    } else {
+    if (this.tests.PLAYING(message) && this.tests.SAME_VC(message)) {
+      return true;
+    } else if (!this.tests.PLAYING(message)) {
       return true;
     }
 
-    return false;
+    if (this.tests.PLAYING(message) && !this.tests.SAME_VC(message)) {
+      this.errorEmbed(message, "different_member_vc");
+      return false;
+    }
+  }
+
+  canStop(message) {
+    if (!this.tests.MEMBER_VC(message)) {
+      this.errorEmbed(message, "no_member_vc");
+      return false;
+    }
+
+    if (!this.tests.PLAYING(message)) {
+      this.errorEmbed("no_player");
+      return false;
+    }
+
+    if (!this.tests.SAME_VC(message)) {
+      this.errorEmbed("different_member_vc");
+      return false;
+    }
+
+    return true;
   }
 
   /**
-   * Generate an embed for an error
+   * Send an embed for an error
+   * @param {CommandoMessage} message the message object
    * @param {string} type The error type
    */
-  errorEmbed(type) {
+  errorEmbed(message, type) {
     const embed = new MessageEmbed().setColor("RED");
 
-    switch (type) {
-      case "no_member_vc":
-        embed.setDescription(
-          "You need to be in a voice channel for this command!"
-        );
-        break;
-
-      case "not_playing":
-        embed.setDescription("Nothing is playing right now!");
-        break;
-
-      case "different_vc":
-        embed.setDescription("You're not in the same voice channel as me!");
-        break;
-
-      default:
-        embed.setDescription(
-          "You're not allowed to use this command for some reason..."
-        );
-        break;
+    if (replies[type]) {
+      embed.setDescription(replies[type]);
+    } else {
+      embed.setDescription(
+        "You're not allowed to use this command for some reason..."
+      );
     }
 
-    return embed;
+    message.embed(embed);
   }
 }
 
