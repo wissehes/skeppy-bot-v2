64345b02da5dc9038d2bc852146f65630e2c6a99
Wisse <wissehes@gmail.com>
2020-10-20 14:28:03 +0200

    Create example config

---
+++ config.example.js
@@ -0,0 +1,17 @@
+module.exports = {
+  token: "",
+  prefix: "skeptest",
+  api: {
+    ksoft: "",
+  },
+  mongoURI: "mongodb://localhost/skepv2",
+
+  lavalinkNodes: [
+    {
+      name: "local",
+      host: "localhost",
+      port: 2333,
+      auth: "youshallnotpass",
+    },
+  ],
+};
