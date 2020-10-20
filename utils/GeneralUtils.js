e4b0aaa4e930cfd05462c965b905f237ad7bb578
Wisse <wissehes@gmail.com>
2020-10-20 15:07:54 +0200

    Add shuffleArray to generalutils

--- utils/GeneralUtils.js
+++ utils/GeneralUtils.js
@@ -7,6 +7,26 @@ class GerneralUtils {
   static randomNumber(min, max) {
     return Math.round(Math.random() * max) + min;
   }
+
+  /**
+   * Shuffles an aray
+   * @param {array} array The array you want to shuffle
+   */
+  static shuffleArray(array) {
+    let counter = array.length;
+
+    while (counter) {
+      let index = Math.floor(Math.random() * counter);
+
+      counter--;
+
+      let temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+
+    return array;
+  }
 }
 
 module.exports = GerneralUtils;
