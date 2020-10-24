df3f2829a6e74fc64d948dde9e98db789f3dba7f
Wisse <wissehes@gmail.com>
2020-10-24 23:11:01 +0200

    add overviewEmbed method to welcome utils

--- utils/WelcomeUtils.js
+++ utils/WelcomeUtils.js
@@ -1,11 +1,13 @@
-const { GuildMember } = require("discord.js");
+const { GuildMember, MessageEmbed } = require("discord.js");
+const { CommandoGuild } = require("discord.js-commando");
+const { stripIndents } = require("common-tags");
 
 const variables = [
-  ["members", (member) => member.guild.memberCount],
-  ["server", (member) => member.guild.name],
-  ["user", (member) => member.user.toString()],
-  ["usertag", (member) => member.user.tag],
-  ["username", (member) => member.user.username],
+  ["members", "Member count", (member) => member.guild.memberCount],
+  ["server", "Server name", (member) => member.guild.name],
+  ["user", "New member mention", (member) => member.user.toString()],
+  ["usertag", "New member tag", (member) => member.user.tag],
+  ["username", "New member username", (member) => member.user.username],
 ];
 
 class WelcomeUtils {
@@ -20,7 +22,7 @@ class WelcomeUtils {
     for (const variable of variables) {
       formattedMessage = formattedMessage.replace(
         `{{${variable[0]}}}`,
-        variable[1](member)
+        variable[2](member)
       );
     }
 
@@ -29,6 +31,44 @@ class WelcomeUtils {
 
   static get variables() {
     return variables;
+  }
+
+  /**
+   * Generate the overview embed for a guild
+   * @param {CommandoGuild} guild
+   */
+  static async overviewEmbed(guild) {
+    const prefix = guild.commandPrefix;
+
+    const status = (await guild.settings.get("welcome", false))
+      ? "✅ Enabled"
+      : "❌ Disabled";
+    const channelID = await guild.settings.get("welcomeChannel", null);
+    const channel = guild.channels.resolve(channelID);
+
+    const welcomeMessage = await guild.settings.get(
+      "welcomeMessage",
+      "Welcome {{user}} to {{server}}!"
+    );
+
+    const embed = new MessageEmbed()
+      .setTitle(`Welcome settings for ${guild.name}`)
+      .setColor("BLUE")
+      .setDescription(
+        stripIndents`**Status:** ${status}
+        **Channel:** ${channel.toString() || `**Not set**`}
+        **Message:** ${welcomeMessage}
+        `
+      )
+      .addField(`Setting a channel`, `\`${prefix}welcome channel <#channel>\``)
+      .addField(`Setting a message`, `\`${prefix}welcome message\``)
+      .addField(`Getting an example`, `\`${prefix}welcome example\``)
+      .addField(
+        `Enabling or disabling welcome messages`,
+        `\`${prefix}welcome <enable/disable>\``
+      );
+
+    return embed;
   }
 }
 
